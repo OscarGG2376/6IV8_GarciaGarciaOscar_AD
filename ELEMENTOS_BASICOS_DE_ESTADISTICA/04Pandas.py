86738ec7ff9cb6307a11df3ffcb46d5f27821457
OscarGG2376 <garcia.garcia.oscar2376@gmail.com>
2025-03-10 16:43:24 -0600

    Pandas

--- ELEMENTOS_BASICOS_DE_ESTADISTICA/04Pandas.py
+++ ELEMENTOS_BASICOS_DE_ESTADISTICA/04Pandas.py
@@ -1,1 +1,45 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../housing.csv')
+
+##mostrar las primeras 5 filas
+print(df.head())
+
+##mostrar las ultimas 5 filas
+print(df.tail())
+
+##mostrar filas especificas
+print(df.iloc[7])
+
+##mostrar la columna ocean-proximity
+print(df['ocean_proximity'])
+
+media = df['total_rooms'].mean()
+print('La media de total room es:', + media)
+
+##mediana
+
+mediana = df['median_house_value'].median()
+print("""La mediana de la columna "valor mediano de la casa" """ , + mediana)
+
+
+##la suma de puopular
+salario = df['population'].sum()
+print('El salario total es de:', + salario)
+
+##filtrar
+
+filtrito = df[df['ocean_proximity'] == 'ISLAND']
+print(filtrito)
+
+#vamos a hacer un grafico de dispersion
+
+plt.scatter(df['ocean_proximity'][:10], df ['median_house_value'][:10])
+
+##nombrado
+
+plt.xlabel('Proximidad')
+plt.ylabel('Precio')
+plt.title('TABLITA')
+plt.show()
